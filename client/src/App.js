4902802456dc10dcf03b7077c98a6ac172698d25
griffy06 <hi2cheena@gmail.com>
2021-03-30 13:57:25 +0530

    react-redux boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './store'
+import { Provider } from 'react-redux'
+import history from './history'
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-      <h1>Hello world</h1>
+      <Navbar />
+      hello wtf
+      {/* <Router history={history}>
+        <Provider store={store}>
+          <Navbar />
+          <Route exact path="/" render={() =>
+              <div>hello</div>
+              // <MainPage />
+            }></Route>
+        </Provider>
+      </Router> */}
     </div>
+    </Provider>
   );
 }
 
