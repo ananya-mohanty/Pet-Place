6740cc87fc050c5d7a501fbdf02531f5aab2f0be
griffy06 <hi2cheena@gmail.com>
2021-04-05 02:48:06 +0530

    adopt page layout

--- client/src/pages/NgosPage.js
+++ client/src/pages/NgosPage.js
@@ -5,14 +5,19 @@ import {
     Button,
     Container,
     Row,
-    Col
+    Col,
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import white from '../images/white.png'
 // import WishModal from '../components/WishModal';
 
 const mainStyle = {
-    marginTop: "8rem"
+    position: "relative",
+    marginTop: "4rem",
+    padding: "3rem"
 }
 
 const JumbotronStyle = {
@@ -24,6 +29,26 @@ const JumbotronStyle = {
     padding: "10px",
     textAlign: "center"
 };
+
+const divStyle = {
+    width: 250, 
+    borderStyle:"solid", 
+    borderWidth: 1, 
+    borderColor: "rgba(0,0,0,0.1)",
+    marginTop: "3.5rem",
+    marginLeft: "3.5rem",
+    padding: "10px",
+    textAlign: "center"
+}
+
+const containerStyle = {
+    width: 1050, /* Can be in percentage also. */
+    height: "auto",
+    margin: "0 auto",
+    padding: 50,
+    position: "relative",
+    background: "white"
+}
 
 const imageStyle = {
     height: "8rem"
@@ -43,20 +68,31 @@ const spanStyle = {
     marginTop: "-1.5rem"
 }
 
-class DisplayNgo extends Component {
+class DisplayDonation extends Component {
     render() {
         return (
-            <div>
-                <Jumbotron style={JumbotronStyle}>
+            // <div> 
+            <div style={divStyle}>
+                {/* <Jumbotron style={JumbotronStyle}> */}
+                    {console.log(this.props.ngo)}
                     {/* <WishModal purpose={'card'} id={this.props.donation._id} /> */}
                     {/* <Button href={"/toydescription/" + this.props.item._id} style={buttonStyle}>View</Button> */}
                     {/* <img src={require(`../${this.props.donation.image}`).default} style={imageStyle}></img> */}
-                    <br /><br /><h5>{this.props.ngo.name}</h5>
+                    {/* <br /><br /><h5>{this.props.donation.description}</h5> */}
                     {/* <h5 style={{ color: 'hotpink' }}>
                         &#8377;{this.props.item.price}
                     </h5><br /> */}
-                </Jumbotron>
-            </div>
+                {/* </Jumbotron> */}
+               
+                    <CardImg top width="50" src={white} alt="Card image cap" />
+                    <CardBody>
+                    <CardTitle tag="h6">Card title</CardTitle>
+                    <CardSubtitle className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button>Button</Button>
+                    </CardBody>
+                </div>
+            // </div>
         )
     }
 }
@@ -86,7 +122,8 @@ export class NgosPage extends Component {
 
         return (
             <div className='container' style={mainStyle}>
-                <h3>All NGOs</h3>
+                <div style={containerStyle}>
+                <i class="fa fa-file-text-o fa-lg" aria-hidden="true" style={{float: "left", marginTop: 4}}></i><h5 style={{fontFamily:"muli"}}> &nbsp; &nbsp;Registered NGOs</h5>
                 <span style={spanStyle}>
                     {/* <Link to="/allitems" className='link'>All Toys </Link>| 
                     <Link to="/stuffedanimals" className='link'> Stuffed Animals </Link>| 
@@ -98,11 +135,11 @@ export class NgosPage extends Component {
                     this.state.Ngos.map((ngo, i) => {
                          return (<div>
                             {
-                                <DisplayNgo ngo={ngo} key={i} />
+                                <DisplayDonation ngo={ngo} key={i} />
                             }
                         </div>)
                     })
-                }</Row>
+                }</Row></div>
             </div>
         )
     }
