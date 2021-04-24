8b63224ca9e6c2926a47230790a70024771130e8
griffy06 <hi2cheena@gmail.com>
2021-04-24 19:46:07 +0530

    chat panel update

--- client/src/components/ChatPanel.js
+++ client/src/components/ChatPanel.js
@@ -4,83 +4,71 @@ import { MessageList, Input, Button } from 'react-chat-elements'
 import 'react-chat-elements/dist/main.css'
 
 export class ChatPanel extends Component {
-    
+
+    state= {
+        msg: {},
+        msgList: [
+            {
+                position: 'right',
+                type: 'text',
+                text: 'Lorem',
+                date: new Date(),
+            },
+            {
+                position: 'right',
+                type: 'text',
+                text: 'Lorem ipsum',
+                date: new Date(),
+            },
+           ]
+    }
+
+    onChange = (e) => {
+        this.setState({msg: {
+            position: 'right',
+            type: 'text',
+            text: e.target.value,
+            date: new Date()
+        }})
+    }
+
+    addMessage = (e) => {
+        this.setState({msgList: [...this.state.msgList, this.state.msg]})
+        this.setState({msg: ''})
+        this.inputRef.clear()
+    }
+
+    inputRef = React.createRef();
+
     render() {
+
         return (
-            <div style={{height: 450, overflow: 'scroll', backgroundColor: '#e5e4e2'}}/*style={{width: 420, height: 600, marginTop: -90, marginLeft: -50, marginRight: -100, backgroundColor:'white'}}*/>
+            <div style={{height: 450, width: 450, overflow: 'scroll', backgroundColor: '#e5e4e2'}}/*style={{width: 420, height: 600, marginTop: -90, marginLeft: -50, marginRight: -100, backgroundColor:'white'}}*/>
             <div style={{height: 50, marginBottom: 10, backgroundColor: 'white'}}></div>
             <MessageList
                 className='message-list'
                 // lockable={true}
                 // downButtonBadge={10}
                 // toBottomHeight={'50%'}
-                dataSource={[
-                    {
-                        position: 'right',
-                        type: 'text',
-                        text: 'Lorem',
-                        date: new Date(),
-                    },
-                    {
-                        position: 'right',
-                        type: 'text',
-                        text: 'Lorem ipsum',
-                        date: new Date(),
-                    },
-                    {
-                        position: 'left',
-                        type: 'text',
-                        text: 'Lorem',
-                        date: new Date(),
-                    },
-                    {
-                        position: 'left',
-                        type: 'text',
-                        text: 'Lorem ips',
-                        date: new Date(),
-                    },
-                    {
-                        position: 'left',
-                        type: 'text',
-                        text: 'Lorem ips',
-                        date: new Date(),
-                    },
-                    {
-                        position: 'left',
-                        type: 'text',
-                        text: 'Lorem ips',
-                        date: new Date(),
-                    },
-                    {
-                        position: 'left',
-                        type: 'text',
-                        text: 'Lorem ipsum dolor sit amet',
-                        date: new Date(),
-                    },
-                    {
-                        position: 'left',
-                        type: 'text',
-                        text: 'Lorem ip',
-                        date: new Date(),
-                    },
-                    {
-                        position: 'left',
-                        type: 'text',
-                        text: 'Lorem',
-                        date: new Date(),
-                    }
-            ]} />
-            <Input
+                dataSource={this.state.msgList} />
+                <div style={{marginTop: 340}}>
+                     <Input
+        
                         placeholder="Write a message.."
-                        defaultValue=""
+                        value=""
                         ref='input'
                         multiline={true}
+                        name='msg'
+                        ref={el => (this.inputRef = el)}
+                        onChange = {this.onChange}
                         // buttonsFloat='left'
                         rightButtons={
                             <Button
                                 text='Send'
-                                /*onClick={() => this.addMessage()}*/ />
+                                onClick={this.addMessage} />
                         } />
+                
+                </div>
             </div>
          )
     }
