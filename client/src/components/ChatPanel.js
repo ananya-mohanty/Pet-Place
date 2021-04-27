36672ce1adf340a5de52e56aa85b89975fc7585f
griffy06 <hi2cheena@gmail.com>
2021-04-28 03:10:06 +0530

    multimedia in chats

--- client/src/components/ChatPanel.js
+++ client/src/components/ChatPanel.js
@@ -1,27 +1,55 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { MessageList, Input, Button } from 'react-chat-elements'
+import { MessageList } from 'react-chat-elements'
+import {Input, InputGroup, InputGroupAddon, InputGroupText, Button, Row, Col, Container} from 'reactstrap'
 import 'react-chat-elements/dist/main.css'
 import axios from 'axios'
 import {getMessages, addMessage} from '../actions/chatAction'
 import PropTypes from 'prop-types'
+import { Redirect } from 'react-router'
+import history from '../history'
 
 export class ChatPanel extends Component {
 
     i=0
     state= {
         msg: {},
-        msgList: []
+        msgList: [],
+        files: [],
+        filesrc: [],
+        ext: null,
+        numfiles: 0,
+        caption: '',
+        filetype: []
     }
 
+    onFileChange = e => {
+        var prev = this.state.numfiles
+        this.state.numfiles += e.target.files.length
+        // console.log(this.state.numfiles)
+        var i=0
+        var src = this.state.filesrc
+        var files=this.state.files
+        var filetype = this.state.filetype
+        while (i<this.state.numfiles-prev)
+        {
+            files.push(e.target.files[i]);
+            filetype.push(e.target.files[i].name.split('.').pop());
+            src.push(URL.createObjectURL(e.target.files[i]))
+            // src.push(`https://localhost:5000/api/messages/${e.target.files[i].name}`)
+            i++
+        }
+        this.setState({filesrc:src, files:files, filetype:filetype})
+    };
+
     componentDidMount=()=>{
-        console.log(this.props.user1)
+        // console.log(this.props.user1)
         this.props.getMessages(this.props.user1)
         const userMessages = this.props.chat.userMessages
         userMessages.map((msg)=>{
-            const{position, type, text}=msg
+            const{position, type, text, data}=msg
             const date = new Date(msg.updatedAt)
-            this.state.msgList.push({position, type, text, date})
+            this.state.msgList.push({position, type, text, date, data})
         })
         
     }
@@ -30,11 +58,12 @@ export class ChatPanel extends Component {
         this.i++
         this.props.getMessages(this.props.user1)
         const userMessages = this.props.chat.userMessages
+        // console.log(userMessages)
         this.state.msgList=[]
         userMessages.map((msg) => {
-            const { position, type, text } = msg
+            const { position, type, text, data } = msg
             const date = new Date(msg.updatedAt)
-            this.state.msgList.push({ position, type, text, date })
+            this.state.msgList.push({ position, type, text, date, data })
         })
         if (this.i < 5) this.scrollRef.scrollIntoView({ behavior: 'smooth' })
     }
@@ -49,43 +78,118 @@ export class ChatPanel extends Component {
 
     addMessage = (e) => {
         e.preventDefault()
-        this.setState({msgList: [...this.state.msgList, this.state.msg]})
-        this.setState({msg: ''})
-        this.inputRef.clear()
-        this.props.addMessage(this.state.msg.text, this.state.position, this.props.user1)
+        this.formRef.reset()
+        if(this.state.files.length == 0 )
+        {
+            this.setState({msgList: [...this.state.msgList, this.state.msg]})
+            this.setState({msg: ''})
+            // this.inputRef.clear()
+            this.props.addMessage(this.state.msg.text, this.state.position, 'text', null, null, this.props.user1)
+        }
+        else
+        {
+            var msglistnew = []
+            var i = 0
+            while(i<this.state.files.length)
+            {
+                if(this.state.filetype[i] == 'png' || this.state.filetype[i] == 'jpeg' || this.state.filetype[i] == 'jpg')
+                {
+                    msglistnew.push({position: this.state.msg.position, type: 'photo', text: this.state.msg.text, uri: this.state.filesrc[i], file: this.state.files[i]})
+                }
+                else if(this.state.filetype[i] == 'mp4' || this.state.filetype[i] == 'ogg' || this.state.filetype[i] == 'webm')
+                {
+                    msglistnew.push({position: this.state.msg.position, type: 'video', text: this.state.msg.text, uri: this.state.filesrc[i], file: this.state.files[i]})
+                }
+                else
+                {
+                    msglistnew.push({position: this.state.msg.position, type: 'file', text: this.state.msg.text, uri: this.state.filesrc[i], file: this.state.files[i]})
+                }
+                i++
+            }
+            this.setState({msgList: [...this.state.msgList, msglistnew], files: [], filesrc: [], filetype: [], msg: ''})
+            // this.setState({})
+            // this.setState({})
+            // this.inputRef.clear()
+            i = 0
+            while(i<msglistnew.length)
+            {
+                this.props.addMessage(msglistnew[i].text, msglistnew[i].position, msglistnew[i].type, msglistnew[i].uri, msglistnew[i].file, this.props.user1)
+                i++
+            }
+            
+        }
+        
     }
 
-    inputRef = React.createRef();
+    formRef = React.createRef();
     scrollRef = React.createRef();
 
     render() {
         return (
             // <div style={{overflow: 'hidden'}}>
+            <div>
             <div style={{ backgroundColor: '#e5e4e2',}} overflow='hidden' /*style={{width: 420, height: 600, marginTop: -90, marginLeft: -50, marginRight: -100, backgroundColor:'white'}}*/>
             <div style={{height: 20, marginBottom: 10, backgroundColor: 'white'}}></div>
             <MessageList
                 className='message-list'
                 // downButtonBadge={10}
                 toBottomHeight={'100%'}
-                dataSource={this.state.msgList} />
-                <div style={{marginTop: 350, position:'sticky'}} ref={el => (this.scrollRef = el)}>
-                     <Input
-                        placeholder="Write a message.."
-                        value=""
-                        ref='input'
-                        multiline={true}
-                        name='msg'
-                        ref={el => (this.inputRef = el)}
-                        onChange = {this.onChange}
-                        // buttonsFloat='left'
-                        rightButtons={
-                            <Button
-                                text='Send'
-                                onClick={this.addMessage} />
-                        } />
-                </div>
+                dataSource={this.state.msgList} 
+                // onClick={(msg) => console.log(msg)}
+                onClick={(msg) => {
+                    console.log(msg.data.uri)
+                        history.push(msg.data.uri)
+                        // history.go(0)
+                    
+                }}/>
+                
+                <div style={{marginTop: 700, position:'sticky'}} ref={el => (this.scrollRef = el)}><form ref={el => (this.formRef = el)}>
+                <InputGroup>
+                    <Input defaultValue="" placeholder="Write something..." onChange={this.onChange}/>
+                    <InputGroupAddon addonType="append" style={{backgroundColor: 'white'}}>
+                            <label for="img" style={{marginTop: 8, marginLeft: 8}}>
+                                <i class="fa fa-image" />
+                            </label>&nbsp;&nbsp;&nbsp;
+                            <label for="vid" style={{marginTop: 8}}>
+                               <i class="fa fa-video-camera" />
+                            </label>&nbsp;&nbsp;&nbsp;
+                            <label for="doc" style={{marginTop: 8}}>
+                                <i class="fa fa-file" />
+                            </label>&nbsp;&nbsp;&nbsp;
+                    <Button color='success' onClick={this.addMessage}>Send</Button>
+                    </InputGroupAddon></InputGroup></form>
+
+                        
+                        <div style={{ float: 'right', position: 'relative', marginTop: '-35px', marginRight: '170px', zIndex: '5' }} >
+                                    {this.state.filesrc.map((src, idx) => {
+                                        return (
+                                        this.state.filetype[idx] == 'png' || this.state.filetype[idx] == 'jpeg' || this.state.filetype[idx] == 'jpg' ?
+                                            <img src={src} style={{
+                                                width: '30px',
+                                                height: '30px', border: '1px solid black', borderRadius: '5px',
+                                                marginLeft: '5px',
+                                                marginBottom: '20px'
+                                            }} /> :
+                                            this.state.filetype[idx] == 'mp4' || this.state.filetype[idx] == 'ogg' || this.state.filetype[idx] == 'webm' ?
+                                                <video 
+                                                        style={{
+                                                            width: '30px',
+                                                            height: '30px', border: '1px solid black', borderRadius: '5px',
+                                                            marginLeft: '5px'}}><source src={src} /></video>:
+                                                    <embed style={{
+                                                        width: '30px',
+                                                        height: '30px', border: '1px solid black', borderRadius: '5px',
+                                                        marginLeft: '5px',
+                                                    }}  name="plugin" src={src} type="application/pdf"/>
+                                    )})}
+                
             </div>
-            // </div>
+            </div></div><div style={{backgroundColor: 'transparent'}}>
+                <input type="file" name='files' id="img" accept="image/*" style={{ visibility: 'hidden' }} onChange={this.onFileChange} multiple />
+                <input type="file" name='files' id="vid" accept="video/*"style={{ visibility: 'hidden' }} onChange={this.onFileChange} multiple />
+                <input type="file" name='files' id="doc" accept="application/*, text/*" style={{ visibility: 'hidden' }} onChange={this.onFileChange} multiple />
+                </div>
+             </div>
          )
     }
 }
--- routes/api/chats.js
+++ routes/api/chats.js
@@ -1,10 +1,111 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const GridFsStorage = require('multer-gridfs-storage');
+const config = require('config');
+const crypto = require('crypto');
+const auth = require('../../middleware/auth');
+var multer = require('multer');
 
 //message model
 const {Message} = require('../../models/Message');
 //user model
 const User = require('../../models/User');
+
+//files
+const storage = new GridFsStorage({
+    url: config.get('mongoURI'),
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                //  const filename = file.originalname;
+                const filename = buf.toString('hex') + path.extname(file.originalname);
+                const original = file.originalname;
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: 'uploads',
+                    metadata: original
+                };
+                resolve(fileInfo);
+            });
+        });
+    }
+});
+const upload = multer({ storage });
+
+
+router.get('/image/:filename', function (req, res) {
+    console.log("idhr")
+    global.gfs.files.findOne({ filename: req.params.filename }, function (err, file) {
+        if (!file || file.length === 0) {
+            return res.status(404).json({
+                err: 'No file exists'
+            })
+        }
+        //check if image
+        if (file.contentType === 'image/jpeg' || file.contentType === 'image/jpg' || file.contentType === 'image/png') {
+            //read output to browser
+            const readStream = gfs.createReadStream(file.filename);
+            readStream.pipe(res);
+        } else {
+            return res.status(404).json({
+                err: 'Not an image'
+            })
+        }
+    })
+})
+
+router.get('/video/:filename', function (req, res) {
+    console.log("popop")
+    global.gfs.files.findOne({ filename: req.params.filename }, function (err, file) {
+        if (!file || file.length === 0) {
+            return res.status(404).json({
+                err: 'No file exists'
+            })
+        }
+
+        //check if image
+        if (file.contentType === 'video/mp4' || file.contentType === 'video/ogg' || file.contentType === 'video/webm') {
+            //read output to browser
+            const readStream = gfs.createReadStream(file.filename);
+            readStream.pipe(res);
+        } else {
+            return res.status(404).json({
+                err: 'Not a video'
+            })
+        }
+    })
+
+
+})
+
+
+router.get('/document/:filename', function (req, res) {
+    global.gfs.files.findOne({ filename: req.params.filename }, function (err, file) {
+        if (!file || file.length === 0) {
+            return res.status(404).json({
+                err: 'No file exists'
+            })
+        }
+        //console.log(file.metadata);
+        //check if image
+        if (file.contentType === 'application/pdf' || file.contentType === 'application/octet-stream' || file.contentType === 'text/plain' || file.contentType === 'application/x-zip-compressed') {
+            //read output to browser
+            const readStream = gfs.createReadStream(file.filename);
+            readStream.pipe(res);
+        } else {
+            return res.status(404).json({
+                err: 'Not a document'
+            })
+        }
+    })
+
+
+})
+
 
 //@route  GET api/messages/:id/:id_user2
 //@desc   Get all messages between this user and user2
@@ -19,7 +120,14 @@ router.get('/:id/:id2', (req, res) => {
                 msgs = user.messages.get(req.params.id2);
             }
             
-                    res.json(msgs)
+                     res.json(msgs)
+                    // global.gfs.files.find().toArray(function (err, files) {
+                    //     if (err) console.log(err);
+                    //     else
+                    //     {
+                    //         res.json({'msgs':msgs, 'files':files})
+                    //     }
+                    // })
         });
 });
 
@@ -27,13 +135,21 @@ router.get('/:id', (req, res)=>{
     User.findById(req.params.id)
         .then(user => {
             var msgs = user.messages
+            // global.gfs.files.find().toArray(function (err, files) {
+            //     if (err) console.log(err);
+            //     else
+            //     {
+            //         res.json({'msgs':msgs, 'files':files})
+            //     }
+            // })
             res.json(msgs)
         });
 })
+
 //@route  POST api/messages/:id/:id2
 //@desc   add a message in chats of this user and user2
 //@access Private
-router.post('/:id/:id2', (req, res) => {
+router.post('/:id/:id2', upload.array('files[]', 10), (req, res) => {
     const t=req.body.text
     const s = t.substring(0, Math.min(t.length, 10))
 
@@ -41,15 +157,43 @@ router.post('/:id/:id2', (req, res) => {
         .then(user => {
             User.findById(req.params.id2)
                 .then(user2 => {
-                    const { text, position } = req.body
+                    const { text, position, type, uri } = req.body
+                    console.log("here")
+                    console.log(type)
+                    var urivar = null
+                    var typevar = type
+                    if(type=='photo'){
+                    urivar = "http://localhost:5000/api/messages/image/" + req.files[0].filename
+                    }
+                    else if(type=='video'){
+                        console.log(uri)
+                        console.log(req.files[0].filename)
+                        try {  
+                        urivar = "http://localhost:5000/api/messages/video/" + req.files[0].filename
+                        } catch(err) {
+                            console.log(err)
+                        }
+                        typevar = 'file'
+                    }
+                    else if(type!='text'){
+                        console.log("here")
+                    urivar = "http://localhost:5000/api/messages/document/" + req.files[0].filename
+                    }
+                    console.log("idhr")
                     const newMessage1 = new Message({
                         sender: user.name,
                         receiver: user2.name,
                         text: text,
                         position: 'right',
-                        subtitle: s
+                        subtitle: s,
+                        type: typevar,
+                        data: {uri: urivar}
                     });
+                    req.files.forEach(function (fileobj) {
+                        newMessage1.files.push(fileobj.id);
+                    })
                     newMessage1.save()
+                    console.log(newMessage1)
                     temp = []
 
                     if (user.messages.has(req.params.id2)) {
@@ -57,18 +201,22 @@ router.post('/:id/:id2', (req, res) => {
                     }
                     temp.push(newMessage1)
                     user.messages.set(req.params.id2, temp)
-                    console.log(user.messages)
+                    // console.log(user.messages)
                     user.markModified('messages')
                     user.save()
                         .then(()=>{
-                    const { text, position } = req.body
                     const newMessage2 = new Message({
                         sender: user.name,
                         receiver: user2.name,
                         text: text,
                         position: 'left',
-                        subtitle: s
+                        subtitle: s,
+                        type: typevar,
+                        data: {uri: urivar}
                     });
+                    req.files.forEach(function (fileobj) {
+                        newMessage2.files.push(fileobj.id);
+                    })
                     newMessage2.save()
                     temp = []
 
@@ -77,10 +225,10 @@ router.post('/:id/:id2', (req, res) => {
                     }
                     temp.push(newMessage2)
                     user2.messages.set(req.params.id, temp)
-                    console.log(user2.messages)
+                    // console.log(user2.messages)
                     user2.markModified('messages')
                     user2.save()
-                        .then(console.log(user2))
+                        .then(/*console.log(user2)*/)
                         .catch(err => console.log(err))
                 })})})
                     .catch(err => console.log(err))})
@@ -110,5 +258,7 @@ router.post('/:id/:id2', (req, res) => {
 //         return res.status(401).json('Please login to make changes to cart')
 //     }
 // }
+
+
 
 module.exports = router;
