d55a9ca7f4b5bd5f70cc35e90d39a3b5560fc7a8
ananya-mohanty <42179851+ananya-mohanty@users.noreply.github.com>
2021-04-27 22:50:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const multer = require('multer');
 const GridFsStorage = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
 const app = express();
-app.use(express.json());
 
 
 //DB config
@@ -54,6 +53,7 @@ const storage = new GridFsStorage({
 });
 global.upload = multer({ storage });
 
+
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/ngo', require('./routes/api/auth2'));
@@ -63,6 +63,9 @@ app.use('/api/contribute', require('./routes/api/contribute'));
 app.use('/api/post', require('./routes/api/post'));
 app.use('/api/lostpet', require('./routes/api/lostpet'));
 app.use('/api/messages', require('./routes/api/chats'));
+app.use('/api/adoption', require('./routes/api/adoption'));
+
+app.use(express.json());
 
 
 const port = process.env.PORT || 5000;
