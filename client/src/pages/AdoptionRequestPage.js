23ea35b75d8297ff9e806a5068ee8370a9aa4db1
ananya-mohanty <42179851+ananya-mohanty@users.noreply.github.com>
2021-04-30 23:52:14 +0530

    Modal for Adoption request forms

--- client/src/pages/AdoptionRequestPage.js
+++ client/src/pages/AdoptionRequestPage.js
@@ -1,204 +1,4 @@
-// import axios from 'axios';
-// import React, { Component } from 'react'
-// import {
-//     Jumbotron,
-//     Button,
-//     Container,
-//     Row,
-//     Col,
-//     Card, CardImg, CardText, CardBody,
-//     CardTitle, CardSubtitle, Modal, ModalBody, ModalHeader
-// } from 'reactstrap';
-// import { connect } from 'react-redux';
-// import AliceCarousel from 'react-alice-carousel';
-// import "react-alice-carousel/lib/alice-carousel.css";
-// import ChatPanel from '../components/ChatPanel';
-// import LostPet from '../components/LostPet'
-// import { Link } from 'react-router-dom'
-// const mainStyle = {
-//     position: "relative",
-//     marginTop: '40px',
-//     padding: "3rem"
-// }
 
-// const divStyle = {
-//     width: 250,
-//     borderStyle: "solid",
-//     borderWidth: 1,
-//     borderColor: "rgba(0,0,0,0.1)",
-//     marginTop: "3.5rem",
-//     marginLeft: "3.5rem",
-//     padding: "10px",
-//     textAlign: "center"
-// }
-
-// const containerStyle = {
-//     // width: 1050, /* Can be in percentage also. */
-
-// }
-// const customers = {
-//     width: 600,
-//     padding: "10px",
-//     borderColor: "rgba(0,2,0,0.1)",
-//     position: "relative",
-//     marginTop: '20px',
-//     overflow: "hidden"
-
-// }
-// const trows = {
-//     width: 20,
-//     padding: "10px",
-//     borderColor: "rgba(0,2,0,0.1)",
-//     // position: "relative",
-//     marginTop: '10px',
-//     textAlign: "center",
-//     borderStyle: "solid",
-//     borderWidth: 1,
-//     borderCollapse: "collapse",
-//     minWidth:210
-
-// }
-// const trows1 = {
-//     width: 20,
-//     padding: "10px",
-//     borderColor: "rgba(0,2,0,0.1)",
-//     // position: "relative",
-//     marginTop: '10px',
-//     textAlign: "center",
-//     borderStyle: "solid",
-//     borderWidth: 1,
-//     borderCollapse: "collapse",
-//     minWidth:300
-
-// }
-// const imageStyle = {
-//     height: "8rem"
-// }
-
-// const buttonStyle = {
-//     border: "None",
-//     borderRadius: "20px",
-//     background: "white",
-//     color: "black",
-//     float: 'right'
-// }
-
-
-// const dpStyle = {
-//     width: 50,
-//     height: 50,
-//     borderRadius: 25,
-//     overflow: "hidden",
-//     alignSelf: 'flex-start',
-// }
-
-// const spanStyle = {
-//     float: "right",
-//     marginTop: "-1.5rem"
-// }
-
-// class DisplayRequests extends Component {
-//     state = {
-//         chatPanel: false
-//     }
-   
-//     onChat = (e) => {
-//         this.setState({ chatPanel: !this.state.chatPanel })
-//     }
-
-//     render() {
-//         return (
-
-//            <div>
-               
-//                     <table style={customers}>
-//                           {/* <tr>
-//                             <th> Post ID</th>
-//                             <th> Name of Applicant</th>
-//                             <th> Status of Application</th>
-//                           </tr> */}
-                        
-//                         <tr>
-//                             <td style={trows1}> {this.props.adopter._id} </td>
-//                             <td style={trows}>{this.props.adopter.name} </td>
-//                             <td style={trows}> {this.props.adopter.status} </td>
-//                             <td style={trows}><Link to={`/chat/${this.props.adopter.userID}`}><Button className="foundBtn" onClick={this.onChat}>Connect</Button></Link></td>
-//                         </tr>
-//                     </table>
-                    
-//                     </div>
-                    
-               
-            
-//         )
-//     }
-// }
-
-
-// export class AdoptionRequest extends Component {
-
-//     state = {
-//         AdoptRequests: [],
-//         files: [],
-//     }
-
-
-
-//     componentDidMount() {
-//         // console.log('requuuu');
-//         axios.get(`../api/adoption/request/${this.props.user_id}`)
-//             .then((res) => {
-
-//                 this.setState({ AdoptRequests: res.data.items})
-
-//             });
-
-//     }
-
-//     render() {
-
-//         return (
-//             <Container>
-//                 <div className='container' style={mainStyle}>
-//                     <div style={{ height: "auto", margin: "0 auto", padding: 50, position: "relative", background: "white", }}> 
-//                         <i class="fa fa-file-text-o fa-lg" aria-hidden="true" style={{ float: "left", marginTop: 4 }}></i><h5 style={{ fontFamily: "muli" }}> &nbsp; &nbsp; Applications to Your Posts </h5>
-                        
-//                         {/* <Button className="register" onClick={this.toggle}>View Your Application</Button> */}
-//                         <span style={spanStyle}>
-//                             {/* <Link to="/allitems" className='link'>All Toys </Link>| 
-//                     <Link to="/stuffedanimals" className='link'> Stuffed Animals </Link>| 
-//                     <Link to="/woodentoys" className='link'> Wooden Toys </Link> */}
-//                         </span><hr />
-                      
-                        
-//                         <Row>
-//                         <table style={customers}>
-//                           <tr>
-//                             <th style={trows1}> Application ID</th>
-//                             <th style={trows}> Name of Applicant</th>
-//                             <th style={trows}> Status of Application</th>
-//                             <th style={trows}> Connect with Owner</th>
-//                           </tr>
-//                         </table>
-//                             {
-//                                 this.state.AdoptRequests.map((adopter, i) => {
-//                                     // var files = this.state.files.filter((f) => lostpet.files.includes(f._id))
-//                                     return (<div>
-//                                         {
-//                                             <DisplayRequests adopter={adopter} key={i} onClick={this.onClick} />
-                                          
-//                                         }
-                                         
-//                                     </div>)
-//                                 })
-//                             }</Row></div>
-//                 </div>
-//             </Container>
-//         )
-//     }
-// }
-
-// export default connect()(AdoptionRequest)
 import axios from 'axios';
 import React, { Component } from 'react'
 import {
@@ -257,7 +57,7 @@ const trows = {
     borderStyle: "solid",
     borderWidth: 1,
     borderCollapse: "collapse",
-    minWidth:40
+    minWidth:150
 
 }
 const trows1 = {
@@ -302,7 +102,8 @@ const spanStyle = {
 class DisplayRequests extends Component {
     state = {
         chatPanel: false,
-        postID:''
+        postID:'',
+        viewApplication:false
     }
    
     onChat = (e) => {
@@ -321,43 +122,130 @@ class DisplayRequests extends Component {
         axios.put(`/api/request`, {formData}
         );         
     }
+    onSubmit = (e) =>{
+
+    }
+    onView = (e) => {
+        this.setState({ viewApplication: !this.state.viewApplication })
+        console.log("open form")
+    }
     render() {
        
         console.log('HI '+ this.props.adopter)
         return (
 
            <div>
+                <Modal
+                    style={{}}
+                    isOpen={this.state.viewApplication}
+                    toggle={this.toggle}>
+                         <ModalHeader toggle={this.onView}> Adoption Application</ModalHeader>
+                    <ModalBody style={{
+                        paddingTop: '20px',
+                        paddingBottom: '0px',
+                        display: "flex",
+                        backgroundColor: 'white'}}>
+                        {/* <img src={profilepic} style={imageStyle}></img> */}
+                      <div>
+                          <Table>
+
+                         
+                        <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}>Name of the Applicant : 
+                           </th>
+                           <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}>
+                            {this.props.adopter.name} 
+                            </th>
+                            </Table>
+                            
+
+                            <Table>
+                        <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}>Age : 
+                           </th>
+                           <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}>
+                            {this.props.adopter.age} 
+                            </th>
+                            </Table>
+                            
+
+                            <Table> 
+                        <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}>Sex : 
+                           </th>
+                           <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}>
+                            {this.props.adopter.sex} 
+                            </th>
+                            </Table>
+
+                            <Table> 
+                        <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}> Reason for Adoption : 
+                           </th>
+                           <tr style={{ marginLeft: '25px', position: 'relative', zIndex: '1' }}>
+                            <td  style={{ marginLeft: '25px', position: 'relative', zIndex: '1' }} >{this.props.adopter.description} </td>
+                            </tr>
+                            </Table>
+
+                            <Table> 
+                            <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}>Marital Status : 
+                            </th>
+                            <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}>
+                            {this.props.adopter.marital_status} 
+                            </th>
+
+                            </Table>
+                            <Table> 
+                            <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}>Residential Address : 
+                            </th>
+                            <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}>
+                            {this.props.adopter.address} 
+                            </th>
+                            </Table>
+
+                            <Table> 
+                            <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}>Annual Income: 
+                            </th>
+                            <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}>
+                            {this.props.adopter.annualIncome} 
+                            </th>
+                            </Table>
+                            <Table> 
+
+                            <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}>Status of Application : 
+                            </th>
+                            <th style={{ marginLeft: '15px', position: 'relative', zIndex: '1' }}>
+                            {this.props.adopter.status} 
+                            </th>
+                            
+                            </Table>
+                            <div style={{ float: 'right', position: 'relative', marginTop: '-40px', marginRight: '20px', zIndex: '2' }} >
+                                
+            
+                                <br></br>
+                                    <button style={{ marginLeft: '100px' }} type="submit" onClick={this.onSubmit}>Approve Adoption</button>
+                            </div>
+                       
+                            
+                        </div>
+                       
+                    </ModalBody>
+                </Modal>
                
                     <Table hover >
                          
                         <tbody>
-                            {/* <td style={trows1}> {this.props.adopter._id} </td>
-                            <td style={trows}>{this.props.adopter.name} </td>
-                            <td style={trows}> {this.props.adopter.age}</td>
-                            <td style={trows}> {this.props.adopter.sex}</td>
-                            <td style={trows}> {this.props.adopter.marital_status}</td>
-                            <td style={trows}> {this.props.adopter.address}</td>
-                            <td style={trows}>{this.props.adopter.description}</td>
-                            <td style={trows}> {this.props.adopter.status} </td>
-                            <td style={trows}><Link to={`/chat/${this.props.adopter.userID}`}><Button className="foundBtn" onClick={this.onChat}>Connect</Button></Link></td> */}
-
+                            
                             <tr>
-                            <td> {this.props.adopter._id} </td>
-                            <td>{this.props.adopter.name} </td>
-                            <td> {this.props.adopter.age}</td>
-                            <td> {this.props.adopter.sex}</td>
-                            <td> {this.props.adopter.marital_status}</td>
-                            <td> {this.props.adopter.address}</td>
-                            <td>{this.props.adopter.description}</td>
-                            <td> { this.props.adopter.status} 
-                            <Button className="foundBtn" onClick={this.onApprove}>Approve</Button>
+                            <td style={trows1}> {this.props.adopter._id} </td>
+                            <td style={trows}>{this.props.adopter.name} </td>
+                            <td style={trows}> { this.props.adopter.status} </td>
+                            <td style={trows}>
+                            <Button className="foundBtn" onClick={this.onView}>View </Button>
                             </td>
-                            <td><Link to={`/chat/${this.props.adopter.userID}`}><Button className="foundBtn" onClick={this.onChat}>Connect</Button></Link></td>
+                            <td style = {trows}><Link to={`/chat/${this.props.adopter.userID}`}><Button className="foundBtn" onClick={this.onChat}>Connect</Button></Link></td>
                             </tr>
                         </tbody>
                     </Table>
                     
                     </div>
+                    
                     
                
             
@@ -403,12 +291,14 @@ export class AdoptionLists extends Component {
                           <tr>
                             <th style={trows1}> Application ID</th>
                             <th style={trows}> Name of Applicant</th>
-                            <th style={trows}> Age</th>
+                            {/* <th style={trows}> Age</th>
                             <th style={trows}> Sex</th>
                             <th style={trows}> Marital Status</th>
                             <th style={trows}> Residence</th>
-                            <th style={trows}> Reason for Adoption</th>
+                            <th style={trows}> Reason for Adoption</th> */}
+                            
                             <th style={trows}> Status of Application</th>
+                            <th style={trows}> View Application</th>
                             <th style={trows}> Connect with Applicant</th>
                           </tr>
                         </table>
@@ -423,7 +313,8 @@ export class AdoptionLists extends Component {
                                          
                                     </div>)
                                 })
-                            }</Row></div>
+                            }</Row>
+                            </div>
                 </div>
             </Container>
         )
